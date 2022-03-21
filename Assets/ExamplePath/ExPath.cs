55fc8023f2f733b1e2804b63c1bd3c220628e32d
aldetkov <68653581+aldetkov@users.noreply.github.com>
2022-03-21 13:55:25 +0300

    Initial commit

---
+++ Assets/ExamplePath/ExPath.cs
@@ -0,0 +1,44 @@
+﻿using AxGrid.Base;
+using AxGrid.Path;
+using UnityEngine;
+
+namespace ExamplePath
+{
+    public class ExPath : MonoBehaviourExt
+    {
+        [SerializeField] private float timeAnim = 1f;
+        [SerializeField] private float minValue = 0f;
+        [SerializeField] private float maxValue = 100f;
+        
+        private float actionCount = 0;
+        
+        [OnDelay(1f)]
+        private void StartPath()
+        {
+            Path = new CPath();
+
+            Path.Action(ActionPrintLog)
+                .Action(() => print("START EasingLinear"))
+                .EasingLinear(timeAnim, minValue, maxValue, value =>
+                {
+                    print($"{value:##0.00}");
+                })
+                .Action(() =>
+                {
+                    print("END EasingLinear");
+                    print("START EasingCircEaseIn");
+                })
+                .EasingCircEaseIn(timeAnim, minValue, maxValue, value =>
+                {
+                    print($"{value:##0.00}");
+                })
+                .Action(() => print("END EasingCircEaseIn"));
+        }
+
+        private void ActionPrintLog()
+        {
+            print($"Action = {actionCount}");
+            actionCount++;
+        }
+    }
+}
