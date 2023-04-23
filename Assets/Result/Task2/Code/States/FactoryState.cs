5b8b6b7afad69381e81780c20e55e7e27b4eebd3
vend <8vend8@gmail.com>
2023-04-23 19:34:17 +0900

    added card view; changed factory state

--- Assets/Result/Task2/Code/States/FactoryState.cs
+++ Assets/Result/Task2/Code/States/FactoryState.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using AxGrid.FSM;
 using Result.Task2.Code.Extensions;
+using Result.Task2.Code.View;
 using UnityEngine;
 
 namespace Result.Task2.Code.States
@@ -24,11 +25,12 @@ namespace Result.Task2.Code.States
         private void Create()
         {
             int counter = Model.GetInt(Keys.Counter);
-            counter++;
 
             GameObject prefab = _cards.GetRandomElement();
-            GameObject cardObject = Object.Instantiate(prefab);
+            CardView card = Object.Instantiate(prefab).GetComponent<CardView>();
+            card.InitId($"Card_{counter}");
 
+            counter++;
             Model.Set(Keys.Counter, counter);
         }
     }
---
+++ Assets/Result/Task2/Code/View/CardView.cs
@@ -0,0 +1,12 @@
+﻿using AxGrid.Base;
+
+namespace Result.Task2.Code.View
+{
+    public class CardView : MonoBehaviourExt
+    {
+        public string Id { get; private set; }
+
+        public void InitId(string id) =>
+            Id = id;
+    }
+}
